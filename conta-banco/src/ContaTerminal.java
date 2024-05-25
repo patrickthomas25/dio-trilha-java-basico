334e9d0b17a80a18911bdf4c797caf4098fe6f79
patrickthomas25 <patrick.mb.25@gmail.com>
2024-05-25 16:07:56 -0400

    principais características do projeto conta banco feitas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        System.out.println("Por favor, digite seu nome completo!");
+        String nome = scanner.next();
+
+        System.out.println("Digite o número da Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Agora digite o número da sua conta!");
+        int conta = scanner.nextInt();
+
+        System.out.println("Qual o valor de depósito?");
+        double saldo = scanner.nextDouble();
+
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
     }
 }
