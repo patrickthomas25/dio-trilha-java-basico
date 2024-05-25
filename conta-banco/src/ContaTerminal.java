d1e0aa78c35c0b3b8aa7380b43e68368f21994e7
patrickthomas25 <patrick.mb.25@gmail.com>
2024-05-25 16:10:03 -0400

    atributo sobrenome adicionado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,21 +6,21 @@ public class ContaTerminal {
         //TODO: Conhecer e importar a classe Scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
-        //Exibir as mensagens para o nosso usuário
-        //Obter pela scanner os valores digitados no terminal
+        //Exibir as mensagens para o nosso usuário e obter pela scanner os valores digitados no terminal
         System.out.println("Por favor, digite seu nome completo!");
         String nome = scanner.next();
+        String sobrenome = scanner.next();
 
-        System.out.println("Digite o número da Agência");
+        System.out.println("Digite o número da agência.");
         String agencia = scanner.next();
 
-        System.out.println("Agora digite o número da sua conta!");
+        System.out.println("Agora digite o número da sua conta.");
         int conta = scanner.nextInt();
 
         System.out.println("Qual o valor de depósito?");
         double saldo = scanner.nextDouble();
 
         //Exibir a mensagem conta criada
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
     }
 }
